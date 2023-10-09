c458c90d3fa6c5ff691c4d53262a5e3e1d364945
Raphael moura <raphaelmvb2@gmail.com>
2023-10-09 19:57:06 -0300

    Delete users by DELETE

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,4 +49,12 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // ele pega o endereco do novo objeto que inserimos
 		return ResponseEntity.created(uri).build(); //created retorna o codigo 201, que e o codigo de resposta http de quando se cria um recurso
 	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<UserDTO> delete(@PathVariable String id){ // responseEntity do spring
+		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
