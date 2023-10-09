b7015aa3794f177afd339e66438bc2c9a11a237d
Raphael moura <raphaelmvb2@gmail.com>
2023-10-09 19:52:19 -0300

    Users insertion with POST

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.UserDTO;
@@ -36,5 +40,12 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(obj));	
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){ // responseEntity do spring
+		User obj = service.fromDTO(objDto); //dto convertido para user
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // ele pega o endereco do novo objeto que inserimos
+		return ResponseEntity.created(uri).build(); //created retorna o codigo 201, que e o codigo de resposta http de quando se cria um recurso
+	}
 }
