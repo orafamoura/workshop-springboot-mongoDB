5e1d9f10254ded41fa93162d9a73c3ef5126c9c9
Raphael moura <raphaelmvb2@gmail.com>
2023-10-11 17:41:04 -0300

    Referencing user posts

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // com o lazy garante que os post so vao ser carregados se explicitamente acessa-los, se nao quando carregar um usuario so vai vir os dados padroes do usuario
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -48,7 +54,15 @@ public class User implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
 
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -65,6 +79,8 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 	
